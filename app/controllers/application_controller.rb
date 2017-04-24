c688f6b541deb8317a0439cb61674d8ee76b4daf
cathy <cathynoxo@gmail.com>
2017-04-24 23:51:57 +0800

    debug at require_is_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def require_is_admin
-    if current_user.email.admin?
+    if !current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
